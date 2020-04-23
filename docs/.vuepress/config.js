42a1db4a0cec29e9f787d854f85786d313f1c373
mango mac <1092017732@qq.com>
2020-04-23 10:14:43 +0800

    打卡

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,6 +7,7 @@ module.exports = {
     description: '我的计划',
     themeConfig: {
         //logo: '/favicon.ico',
+        lastUpdated: '最近更新',
         nav: [
             { text: '首页', link: '/' },
             //{ text: '指南', link: '/zh/guide/' }
