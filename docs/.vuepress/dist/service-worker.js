e5ccbef33ee3cc1d24b88559ef681d169380227f
mango mac <1092017732@qq.com>
2020-12-02 11:55:46 +0800

    打卡

--- docs/.vuepress/dist/service-worker.js
+++ docs/.vuepress/dist/service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "a6ca43f320a3b619f79644a48dcb7ecc"
+    "revision": "dc179ab942d2aa356da1efdf67e81960"
   },
   {
     "url": "ahead.jpg",
@@ -42,16 +42,16 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.acb569f9.js",
-    "revision": "92362035a582279ff3837a96dc7b34d8"
+    "url": "assets/js/10.e6a6f69d.js",
+    "revision": "87c99eef9379417825bd9cc14467b284"
   },
   {
-    "url": "assets/js/11.cf1fba5e.js",
-    "revision": "9a3cb448128b2c8e8ae56d210511a612"
+    "url": "assets/js/11.bf5c2793.js",
+    "revision": "37220a88fb8ebba2fb54d05a18626af4"
   },
   {
-    "url": "assets/js/12.fa84fb10.js",
-    "revision": "7cb7659766005d91f34f5afb6ad56ea0"
+    "url": "assets/js/12.0a251158.js",
+    "revision": "9a15aab0a773790b36e2031f8ef8db29"
   },
   {
     "url": "assets/js/13.b693d485.js",
@@ -74,36 +74,32 @@ self.__precacheManifest = [
     "revision": "6ce86141be2ed4b41f0cc58cf1008dde"
   },
   {
-    "url": "assets/js/6.14ab45ab.js",
-    "revision": "952d7d2d04da9abf8ede969fcc2d4a19"
+    "url": "assets/js/6.86837026.js",
+    "revision": "5fadf09698c6a630606fcf12c44e3561"
   },
   {
-    "url": "assets/js/7.3fba38d0.js",
-    "revision": "5ed0b707c03ecc74347d165b20b21159"
+    "url": "assets/js/7.cfe128ef.js",
+    "revision": "e7b9830a0e7904e21c7abf4bd347b821"
   },
   {
-    "url": "assets/js/8.9a508de5.js",
-    "revision": "fc69b6cc9fcd7a4eba62ffe69eb0e0d2"
+    "url": "assets/js/8.bc6c01cd.js",
+    "revision": "c7a70ab0dfb6d1c347b217a9dc583d51"
   },
   {
-    "url": "assets/js/9.046ebe86.js",
-    "revision": "a264c083d3b290eddc2ef664f9cc3c2f"
+    "url": "assets/js/9.f583a9c7.js",
+    "revision": "07b717e300fef49bb766280edc65c190"
   },
   {
-    "url": "assets/js/app.0d489ac2.js",
-    "revision": "853149205d6d0bea51d07592a7b1e726"
+    "url": "assets/js/app.2d8f3627.js",
+    "revision": "288f211f346e3de093d6f43b48cbaa81"
   },
   {
     "url": "index.html",
-    "revision": "307dd8d64acb544f27adeb96902d2d9a"
+    "revision": "0655447263a969ee82020ef23cded800"
   },
   {
     "url": "mk.png",
     "revision": "69feb1ebdf6d0913c171ac8995496231"
-  },
-  {
-    "url": "res/bqr.png",
-    "revision": "4a90ac0068942cd0701b72746c3306cf"
   },
   {
     "url": "res/byy.jpg",
@@ -158,6 +154,10 @@ self.__precacheManifest = [
     "revision": "883ef96201d947f5f473634da3549dc7"
   },
   {
+    "url": "res/develop/bqr.png",
+    "revision": "4a90ac0068942cd0701b72746c3306cf"
+  },
+  {
     "url": "res/dinner.jpg",
     "revision": "875fe5c8110e28f1528122d313ed44c2"
   },
@@ -175,31 +175,31 @@ self.__precacheManifest = [
   },
   {
     "url": "zh/punch-in.html",
-    "revision": "10420e977f2f437da2d6dd667d6d12ef"
+    "revision": "c52400e6e74d233f627b0da87014f6c6"
   },
   {
     "url": "zh/punch-in09.html",
-    "revision": "0b8afa016e389216e4f09117863caa42"
+    "revision": "b2c5530b0e627f35353669404e06f2c8"
   },
   {
     "url": "zh/punch-in10.html",
-    "revision": "2149e8276f71ba3387c4fe356ca74fae"
+    "revision": "cff8d1443635b3b4c0114480c69eae01"
   },
   {
     "url": "zh/punch-in11.html",
-    "revision": "5c444336f17c4f7a0537a8fc327fb7fe"
+    "revision": "33d90d342104cb6b94c788ce7013bf31"
   },
   {
     "url": "zh/punch-in12.html",
-    "revision": "0c99d4b214d369f02c8dca95f19de42b"
+    "revision": "05678745e0b7bb60a891636c5842fc67"
   },
   {
     "url": "zh/punch-in2.html",
-    "revision": "c40f8f5ef4f997f48e2e2e48e3637d16"
+    "revision": "1c106d0b76cd13b728158fedeca0ea9f"
   },
   {
     "url": "zh/task-list.html",
-    "revision": "a93fe3b877bb121f67559504cce1f518"
+    "revision": "dc292a666e3632b63266b500ac796afe"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
