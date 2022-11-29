20e8d13c7988d0cf6fce0ae3c863a4c38f8342d3
mango mac <1092017732@qq.com>
2022-11-29 11:37:33 +0800

    更改主服务器

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 # 上传图片资源到nginx
-scp -r res root@master:/opt/nginx/html
+scp -r res root@node:/opt/nginx/html
 # 修改res文件夹名称为public
-ssh root@master 'mv /opt/nginx/html/res /opt/nginx/html/public'
+ssh root@node 'mv /opt/nginx/html/res /opt/nginx/html/public'
